d046e152ad91b9c5dd532f1d3420c86a7bc11129
David Bonet <webbonet@gmail.com>
2020-04-03 21:45:32 +0200

    fix: fill proxy methods

--- src/system_director.rs
+++ src/system_director.rs
@@ -22,22 +22,30 @@ impl SystemDirector {
 
     pub async fn send_to_actor<A: Actor + Receive<M>, M: Debug + Send + 'static>(
         &self,
-        _actor_id: A::Id,
-        _message: M,
-    ) {}
+        actor_id: A::Id,
+        message: M,
+    ) {
+        self.actor_director.send::<A, M>(actor_id, message).await
+    }
 
     pub async fn call_to_actor<A: Actor + Respond<M>, M: Debug + Send + 'static>(
         &self,
-        _actor_id: A::Id,
-        _message: M,
+        actor_id: A::Id,
+        message: M,
     ) -> Result<<A as Respond<M>>::Response, &str> {
-        Err("Shit")
+        self.actor_director.call::<A, M>(actor_id, message).await
     }
 
-    pub(crate) async fn wait_until_stopped(&self) {}
-    pub(crate) async fn stop(&self) {}
+    pub(crate) async fn wait_until_stopped(&self) {
+        self.actor_director.wait_until_stopped().await
+    }
+
+    pub(crate) async fn stop(&self) {
+        self.actor_director.stop().await
+    }
+
     pub(crate) fn get_statistics(&self) -> Vec<(TypeId, Vec<ActorReport>)> {
-        vec!()
+        self.actor_director.get_statistics()
     }
 }
 
