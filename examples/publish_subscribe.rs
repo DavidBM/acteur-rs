326e14fc565a6598838da78f5cdf683675377c3f
David Bonet <webbonet@gmail.com>
2020-04-12 17:39:37 +0200

    add mising files

---
+++ examples/publish_subscribe.rs
@@ -0,0 +1,48 @@
+use acteur::{Acteur, Notify, Service, ServiceConfiguration, SystemAssistant};
+
+#[derive(Debug)]
+struct EmployeeTaxesCalculator {
+    tax_rate: f32,
+}
+
+#[async_trait::async_trait]
+impl Service for EmployeeTaxesCalculator {
+    async fn initialize(system: &SystemAssistant<Self>) -> (Self, ServiceConfiguration) {
+        let service = EmployeeTaxesCalculator { tax_rate: 0.21 };
+        let service_conf = ServiceConfiguration::default();
+
+        println!("Service starting!");
+
+        // Remember to subscribe in order to receiver messages
+        system.subscribe::<EmployeeSalaryChange>().await;
+        println!("Service subscribed!");
+
+        (service, service_conf)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct EmployeeSalaryChange(f32);
+
+// Subscription uses the trait Notify, as normal message sends
+#[async_trait::async_trait]
+impl Notify<EmployeeSalaryChange> for EmployeeTaxesCalculator {
+    async fn handle(&self, message: EmployeeSalaryChange, _: &SystemAssistant<Self>) {
+        println!("Message received! {:?}", message);
+
+        // Calculate the tax rate and send it to however you want
+    }
+}
+
+fn main() {
+    let mut sys = Acteur::new();
+
+    sys.preload_service_sync::<EmployeeTaxesCalculator>();
+
+    // You just need to send the message, whoever is subscribed will received.
+    sys.publish_sync(EmployeeSalaryChange(55000.0));
+
+    sys.stop();
+
+    sys.wait_until_stopped();
+}
---
+++ src/services/broker.rs
@@ -0,0 +1,96 @@
+use crate::services::director::ServicesDirector;
+use crate::services::handle::Notify;
+use crate::services::service::Service;
+use async_std::sync::Arc;
+use dashmap::{mapref::entry::Entry, DashMap};
+use std::any::Any;
+use std::any::TypeId;
+use std::fmt::Debug;
+use std::marker::PhantomData;
+
+#[derive(Debug, Clone)]
+pub struct MessageBroker {
+    managers: Arc<DashMap<TypeId, Vec<Box<dyn ServiceManagerPublish>>>>,
+    system_director: Box<ServicesDirector>,
+}
+
+impl MessageBroker {
+    pub(crate) fn new(system_director: ServicesDirector) -> MessageBroker {
+        MessageBroker {
+            managers: Arc::new(DashMap::new()),
+            system_director: Box::new(system_director),
+        }
+    }
+
+    pub(crate) fn register<S: Service + Notify<M>, M: Sync + Send + Debug + 'static>(&self) {
+        let type_id = TypeId::of::<M>();
+
+        let managers_entry = self.managers.entry(type_id);
+
+        match managers_entry {
+            Entry::Occupied(mut entry) => {
+                entry
+                    .get_mut()
+                    .push(Box::new(ServiceManagerWrapper::<S, M>::new()));
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(vec![Box::new(ServiceManagerWrapper::<S, M>::new())]);
+            }
+        };
+    }
+
+    pub(crate) async fn publish<M: Send + Clone + 'static>(&self, message: M) {
+        let type_id = TypeId::of::<M>();
+
+        if let Some(mut managers) = self.managers.get_mut(&type_id) {
+            for manager in managers.value_mut() {
+                let message = message.clone();
+                manager.send(Box::new(message), &self.system_director).await;
+            }
+        }
+    }
+}
+
+#[async_trait::async_trait]
+trait ServiceManagerPublish: Send + Sync {
+    async fn send(&mut self, message: Box<(dyn Any + Send)>, system_director: &ServicesDirector);
+}
+
+#[derive(Debug)]
+struct ServiceManagerWrapper<S: Service + Notify<M>, M: Debug> {
+    phantom_service: PhantomData<S>,
+    phantom_message: PhantomData<M>,
+}
+
+impl<S: Service + Notify<M>, M: Debug + Send + 'static> ServiceManagerWrapper<S, M> {
+    fn new() -> ServiceManagerWrapper<S, M> {
+        ServiceManagerWrapper {
+            phantom_service: PhantomData,
+            phantom_message: PhantomData,
+        }
+    }
+
+    async fn send(&mut self, message: M, system_director: &ServicesDirector) {
+        system_director.send::<S, M>(message).await;
+    }
+}
+
+#[async_trait::async_trait]
+impl<S: Service + Notify<M>, M: Debug + Send + Sync + 'static> ServiceManagerPublish
+    for ServiceManagerWrapper<S, M>
+{
+    async fn send(&mut self, message: Box<(dyn Any + Send)>, system_director: &ServicesDirector) {
+        match message.downcast::<M>() {
+            Ok(message) => {
+                ServiceManagerWrapper::send(self, *message, system_director).await;
+            }
+            Err(_) => unreachable!(),
+        }
+    }
+}
+
+impl Debug for dyn ServiceManagerPublish {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "ServiceManagerPublish ()")
+    }
+}
